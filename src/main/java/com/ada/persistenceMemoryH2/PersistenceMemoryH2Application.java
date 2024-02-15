2e40f3ee53961d6944ad1b1a2a073c16c459424b
lgar000 <laura.garcia-a@mail.escuelaing.edu.co>
2024-02-14 23:01:10 -0500

    spring boot h2

---
+++ src/main/java/com/ada/persistenceMemoryH2/PersistenceMemoryH2Application.java
@@ -0,0 +1,12 @@
+package com.ada.persistenceMemoryH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceMemoryH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceMemoryH2Application.class, args);
+	}
+}
---
+++ src/test/java/com/ada/persistenceMemoryH2/PersistenceMemoryH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.persistenceMemoryH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceMemoryH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
